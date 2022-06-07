72796a9411fb18f1eb58f1fefa8337e27ed4e26c
eswaribala <parameswaribala@gmail.com>
2022-06-07 20:35:50 +0530

    updated

---
+++ VehicleAPI/VehicleAPI/Contexts/InsuranceContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehicleAPI.Models;
+
+namespace VehicleAPI.Contexts
+{
+    public class InsuranceContext:DbContext
+    {
+
+        public InsuranceContext(DbContextOptions<InsuranceContext> options) 
+            : base(options){
+
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+    }
+}

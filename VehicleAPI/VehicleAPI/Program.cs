6ccfe2ee77b9f6b59585ba0d8f531ee54c70c620
eswaribala <parameswaribala@gmail.com>
2022-06-07 20:46:08 +0530

    updated

--- VehicleAPI/VehicleAPI/Program.cs
+++ VehicleAPI/VehicleAPI/Program.cs
@@ -1,6 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using VehicleAPI.Contexts;
 
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
+
+builder.Services.AddDbContext<InsuranceContext>(options => 
+options.UseSqlServer(configuration.GetConnectionString("Insurance_Conn_String")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

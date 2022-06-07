b867a3919d8c0ad538d07a94130a023e9b414725
eswaribala <parameswaribala@gmail.com>
2022-06-07 19:56:34 +0530

    updated

---
+++ VehicleAPI/VehicleAPI/Models/Maker.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VehicleAPI.Models
+{
+    [Owned]
+    public class Maker
+    {
+        [Column("Brand_Name")]
+        public string? BrandName { get; set; }
+        [Column("Model_No")]
+        public string? ModelNo { get; set; }
+    }
+}
---
+++ VehicleAPI/VehicleAPI/Models/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VehicleAPI.Models
+{
+    public enum FuelType { PETROL, DIESEL,GAS}
+    [Table("Vehicle")]
+    public class Vehicle
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Engine_No" )]
+        public long EngineNo { get; set; }
+        [Column("Rgistration_No")]
+        [Required]
+        public string? RegistrationNo { get; set; }
+        public Maker? Maker { get; set; }
+        [DataType(DataType.Date)]
+        [Column("DOR")]
+        public DateTime DOR { get; set; }
+        [Column("Chassis_No")]
+        public string? ChassisNo { get; set; }
+        
+        [Column("Fuel_Type")]
+        public FuelType FuelType { get; set; }
+        [Column("Color")]
+        public string? Color { get; set; }
+    }
+}

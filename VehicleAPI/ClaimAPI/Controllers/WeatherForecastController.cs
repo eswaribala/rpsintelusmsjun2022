71d6791bd27bdfbd348b0d88a076c40c7122f8d8
eswaribala <parameswaribala@gmail.com>
2022-06-16 21:16:20 +0530

    updated

--- VehicleAPI/ClaimAPI/Controllers/WeatherForecastController.cs
+++ VehicleAPI/ClaimAPI/Controllers/WeatherForecastController.cs
@@ -8,23 +8,26 @@ namespace ClaimAPI.Controllers
     public class WeatherForecastController : ControllerBase
     {
         //private readonly IInvokeService _invokeService;
+        private readonly ILogger<WeatherForecastController> _logger;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            this._logger = logger;
+        }
 
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+       
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-            
-        }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            Random random = new Random();
+            var randomValue = random.Next(0, Summaries.Length);
+            _logger.LogInformation($"Random Value is {randomValue}");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
@@ -33,6 +36,23 @@ namespace ClaimAPI.Controllers
             })
             .ToArray();
         }
-        
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string ThrowErrorMessage(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                    throw new Exception($"id cannot be less than or equal to o. value passed is {id}");
+                return id.ToString();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
+            return string.Empty;
+        }
+
     }
 }
--- VehicleAPI/ClaimAPI/Program.cs
+++ VehicleAPI/ClaimAPI/Program.cs
@@ -1,7 +1,16 @@
 using ClaimAPI.Services;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 using Steeltoe.Discovery.Client;
 var builder = WebApplication.CreateBuilder(args);
-ConfigurationManager configuration = builder.Configuration;
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile(
+        $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+        optional: true)
+    .Build();
+
 // Add services to the container.
 builder.Services.AddScoped<IInvokeService, InvokeService>();
 builder.Services.AddHttpClient<IInvokeService, InvokeService>(options =>
@@ -14,6 +23,22 @@ builder.Services.AddDiscoveryClient(configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+
+    .WriteTo.Debug()
+    .WriteTo.Console()
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new
+        Uri(configuration["ElasticConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"IntelOrderIndex-{DateTime.UtcNow:yyyy-MM}"
+    })
+    .Enrich.WithProperty("Environment", environment)
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
